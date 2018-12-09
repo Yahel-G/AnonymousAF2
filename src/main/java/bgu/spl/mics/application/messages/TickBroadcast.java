b08e3f973bf1aaf859245e727d2aaa14599bd57c
Yahel-G <171717+Yahel-G@users.noreply.github.com>
2018-12-09 14:40:40 +0200

    testing

--- src/main/java/bgu/spl/mics/application/messages/TickBroadcast.java
+++ src/main/java/bgu/spl/mics/application/messages/TickBroadcast.java
@@ -7,12 +7,18 @@ import bgu.spl.mics.Broadcast;
  */
 public class TickBroadcast implements Broadcast {
     private int time;
+    private int timeOfDeath;
 
-    public TickBroadcast(int time){
+    public TickBroadcast(int time, int duration){
         this.time = time;
+        timeOfDeath = duration;
     }
 
     public int giveMeSomeTime(){
         return time;
+    }
+
+    public int getTimeOfDeath() {
+        return timeOfDeath;
     }
 }
